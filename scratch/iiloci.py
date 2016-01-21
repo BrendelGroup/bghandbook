514417406c65e510680260e43af6452e586d25c9
Daniel Standage <daniel.standage@gmail.com>
2016-01-21 12:13:58 -0500

    Improve ziLocus handling, add documentation

--- scratch/iiloci.py
+++ scratch/iiloci.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import argparse
 import collections
+import re
 import sys
 
 def get_parser():
@@ -13,13 +14,22 @@ def get_parser():
 
 
 def iilocus_length_from_gff3(infile, n=2):
+    """
+    Compute aggregate length of n consecutive iiLoci.
+
+    In LocusPocus-produced GFF3, the length of each iiLocus is reported twice:
+    in the `riil` attribute of the giLocus to the left, and the `liil` attribute
+    of the giLocus to the right. This includes zero-length iiLoci (ziLoci),
+    which are not otherwise explicitly reported in the GFF3 file.
+    """
     iiloci = collections.deque()
     for line in infile:
-        if '\tlocus\t' not in line or 'child_gene=' in line:
+        if '\tlocus\t' not in line or 'liil=' not in line:
             continue
-        fields = line.split('\t')
-        seqid = fields[0]
-        ilen = int(fields[4]) - int(fields[3]) + 1
+        seqid = line.split('\t')[0]
+        liilmatch = re.search('liil=(\d+)', line)
+        assert liilmatch, line
+        ilen = int(liilmatch.group(1))
         if len(iiloci) > 0 and iiloci[0][0] != seqid:
             iiloci = collections.deque()
         iiloci.append((seqid, ilen))
